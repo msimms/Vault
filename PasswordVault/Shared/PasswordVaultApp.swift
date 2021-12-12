38460fd5cbdd89d8dbf810b4a7002381d6746f21
Mike Simms <msimms01@gmail.com>
2021-12-12 11:56:46 -0500

    Initial commit.

---
+++ PasswordVault/Shared/PasswordVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordVaultApp.swift
+//  Shared
+//
+//  Created by Michael Simms on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct PasswordVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
