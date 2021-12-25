41a1335ef8fed6f31063a7d95b7e9e3b5d79c41f
Mike Simms <msimms01@gmail.com>
2021-12-24 19:27:55 -0500

    Code cleanup.

--- PasswordVault/Shared/AES.swift
+++ PasswordVault/Shared/AES.swift
@@ -15,7 +15,7 @@ enum AESError: Error {
 }
 
 // The iv is prefixed to the encrypted data
-func aesCBCEncrypt(data:Data, keyData:Data) throws -> Data {
+func aesCBCEncrypt(data: Data, keyData: Data) throws -> Data {
 	let keyLength = keyData.count
 	let validKeyLengths = [kCCKeySizeAES128, kCCKeySizeAES192, kCCKeySizeAES256]
 	if (validKeyLengths.contains(keyLength) == false) {
@@ -26,26 +26,26 @@ func aesCBCEncrypt(data:Data, keyData:Data) throws -> Data {
 	let cryptLength = size_t(ivSize + data.count + kCCBlockSizeAES128)
 	var cryptData = Data(count:cryptLength)
 
-	let status = cryptData.withUnsafeMutableBytes {ivBytes in
-		SecRandomCopyBytes(kSecRandomDefault, kCCBlockSizeAES128, ivBytes)
+	let status = cryptData.withUnsafeMutableBytes {
+		ivBytes in SecRandomCopyBytes(kSecRandomDefault, kCCBlockSizeAES128, ivBytes)
 	}
 	if (status != 0) {
 		throw AESError.IVError(("IV generation failed", Int(status)))
 	}
 
-	var numBytesEncrypted :size_t = 0
-	let options   = CCOptions(kCCOptionPKCS7Padding)
+	var numBytesEncrypted : size_t = 0
+	let options = CCOptions(kCCOptionPKCS7Padding)
 
-	let cryptStatus = cryptData.withUnsafeMutableBytes {cryptBytes in
-		data.withUnsafeBytes {dataBytes in
-			keyData.withUnsafeBytes {keyBytes in
+	let cryptStatus = cryptData.withUnsafeMutableBytes { cryptBytes in
+		data.withUnsafeBytes { dataBytes in
+			keyData.withUnsafeBytes { keyBytes in
 				CCCrypt(CCOperation(kCCEncrypt),
 						CCAlgorithm(kCCAlgorithmAES),
 						options,
 						keyBytes, keyLength,
 						cryptBytes,
 						dataBytes, data.count,
-						cryptBytes+kCCBlockSizeAES128, cryptLength,
+						cryptBytes + kCCBlockSizeAES128, cryptLength,
 						&numBytesEncrypted)
 			}
 		}
@@ -62,7 +62,7 @@ func aesCBCEncrypt(data:Data, keyData:Data) throws -> Data {
 }
 
 // The iv is prefixed to the encrypted data
-func aesCBCDecrypt(data:Data, keyData:Data) throws -> Data? {
+func aesCBCDecrypt(data: Data, keyData: Data) throws -> Data? {
 	let keyLength = keyData.count
 	let validKeyLengths = [kCCKeySizeAES128, kCCKeySizeAES192, kCCKeySizeAES256]
 	if (validKeyLengths.contains(keyLength) == false) {
@@ -73,18 +73,18 @@ func aesCBCDecrypt(data:Data, keyData:Data) throws -> Data? {
 	let clearLength = size_t(data.count - ivSize)
 	var clearData = Data(count:clearLength)
 
-	var numBytesDecrypted :size_t = 0
-	let options   = CCOptions(kCCOptionPKCS7Padding)
+	var numBytesDecrypted : size_t = 0
+	let options = CCOptions(kCCOptionPKCS7Padding)
 
-	let cryptStatus = clearData.withUnsafeMutableBytes {cryptBytes in
-		data.withUnsafeBytes {dataBytes in
-			keyData.withUnsafeBytes {keyBytes in
+	let cryptStatus = clearData.withUnsafeMutableBytes { cryptBytes in
+		data.withUnsafeBytes { dataBytes in
+			keyData.withUnsafeBytes { keyBytes in
 				CCCrypt(CCOperation(kCCDecrypt),
 						CCAlgorithm(kCCAlgorithmAES128),
 						options,
 						keyBytes, keyLength,
 						dataBytes,
-						dataBytes+kCCBlockSizeAES128, clearLength,
+						dataBytes + kCCBlockSizeAES128, clearLength,
 						cryptBytes, clearLength,
 						&numBytesDecrypted)
 			}
