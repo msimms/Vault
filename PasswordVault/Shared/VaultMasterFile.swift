79d480be8053c9dcde2eab4d9f3a3dc4e5cb5b38
Mike Simms <msimms01@gmail.com>
2022-02-18 18:24:50 -0500

    Comments.

--- PasswordVault/Shared/VaultMasterFile.swift
+++ PasswordVault/Shared/VaultMasterFile.swift
@@ -9,18 +9,16 @@ import SwiftUI
 import UniformTypeIdentifiers
 
 struct TextFile: FileDocument {
-	// tell the system we support only plain text
-	static var readableContentTypes = [UTType.plainText]
+	static var readableContentTypes = [UTType.json]
 
-	// by default our document is empty
 	var text = ""
 
-	// a simple initializer that creates new, empty documents
+	/// A simple initializer that creates new, empty documents
 	init(initialText: String = "") {
 		text = initialText
 	}
 
-	// this initializer loads data that has been saved previously
+	/// This initializer loads data that has been saved previously
 	init(configuration: ReadConfiguration) throws {
 		if let data = configuration.file.regularFileContents {
 			text = String(decoding: data, as: UTF8.self)
@@ -29,7 +27,7 @@ struct TextFile: FileDocument {
 		}
 	}
 
-	// this will be called when the system wants to write our data to disk
+	/// This will be called when the system wants to write our data to disk
 	func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
 		let data = Data(text.utf8)
 		return FileWrapper(regularFileWithContents: data)
