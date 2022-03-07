027e0f276985f634afa53e26da243a86af3831ad
Mike Simms <msimms01@gmail.com>
2022-03-07 14:08:44 -0500

    Backup commit.

---
+++ PasswordVault/Shared/PasswordVaultDocument.swift
@@ -0,0 +1,39 @@
+//
+//  PasswordVaultDocument.swift
+//  Shared
+//
+//  Created by Michael Simms on 2/5/22.
+//
+
+import SwiftUI
+import UniformTypeIdentifiers
+
+extension UTType {
+    static var exampleText: UTType {
+        UTType(importedAs: "com.example.plain-text")
+    }
+}
+
+struct PasswordVaultDocument: FileDocument {
+    var text: String
+
+    init(text: String = "Hello, world!") {
+        self.text = text
+    }
+
+    static var readableContentTypes: [UTType] { [.exampleText] }
+
+    init(configuration: ReadConfiguration) throws {
+        guard let data = configuration.file.regularFileContents,
+              let string = String(data: data, encoding: .utf8)
+        else {
+            throw CocoaError(.fileReadCorruptFile)
+        }
+        text = string
+    }
+    
+    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+        let data = text.data(using: .utf8)!
+        return .init(regularFileWithContents: data)
+    }
+}
