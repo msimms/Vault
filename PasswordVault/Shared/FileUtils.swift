3c841a13dc064e552a098a4e920b91c079c3f25a
Mike Simms <msimms01@gmail.com>
2022-04-29 12:13:14 -0400

    Removed dead code.

--- PasswordVault/Shared/FileUtils.swift
+++ PasswordVault/Shared/FileUtils.swift
@@ -29,19 +29,6 @@
 
 import Foundation
 
-func writeToFile(fileName: String, writeText: String) -> Bool {
-	let desktopURL = try! FileManager.default.url(for: .desktopDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-	let fileURL = desktopURL.appendingPathComponent(fileName).appendingPathExtension("txt")
-
-	do {
-		try writeText.write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
-	} catch let error as NSError {
-		print("Error: Failed to write: \n\(error)" )
-		return false
-	}
-	return true
-}
-
 func contentsOfDir(dirName: URL) -> [URL] {
 	let fileManager = FileManager.default
 
